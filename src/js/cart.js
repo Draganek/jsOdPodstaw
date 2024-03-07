38373efd3ca0ba564f60bd262591d4d08ff36472
Draganek <Radek.Draganek@gmail.com>
2024-03-07 20:56:12 +0100

    some changes and learning

---
+++ src/js/cart.js
@@ -0,0 +1,75 @@
+const product1 = { price: 10, title: "JS od podstaw" };
+const product2 = { price: "20", title: "PHP od podstaw" };
+const discount = 10;
+let discountEnabled = false;
+
+const discountElement = document.querySelector("#discount");
+const discountCheckbox = document.querySelector("#add-discount");
+const itemsContainer = document.querySelector("#items");
+
+let counter = 1;
+const addItem = (item) => {
+  itemsContainer.innerHTML += `<tr>
+  <td><button class="delete">x</button></td>
+  <td>${item.title}</td>
+  <td><input type="number" class="quantity" value="1"></td>
+  <td>${item.price}</td>
+  </tr>`;
+}
+
+addItem(product1);
+addItem(product2);
+
+const removeRow = (e) => {
+  if (e.target.tagName === "BUTTON") {
+    const row = e.target.closest("tr");
+    row.remove();
+  }
+}
+
+const removeRowFromQuantity = (e) => {
+  if (Number(e.target.value) === 0) {
+    const row = e.target.closest("tr");
+    row.remove();
+  }
+}
+
+const markBg = (e) => {
+  if (e.target.tagName === "TD") {
+    e.target.closest("tr").classList.toggle("marked");
+  }
+}
+
+//dodaj zniżkę
+const addDiscount = (e) => {
+  discountEnabled = e.target.checked;
+  if (discount) {
+    document.querySelector("#discount-amount").innerHTML = -discount;
+    discountElement.classList.toggle("hidden");
+  }
+  calculatePrice();
+}
+
+// cena całkowita
+const calculatePrice = () => {
+  let total = Number(product1.price) + Number(product2.price);
+  if (discountEnabled) {
+    total -= discount;
+  }
+  document.querySelector("#total-price").innerHTML = total;
+}
+
+calculatePrice();
+
+discountCheckbox.addEventListener("click", addDiscount);
+itemsContainer.addEventListener("click", markBg);
+itemsContainer.addEventListener("click", removeRow);
+itemsContainer.addEventListener("change", removeRowFromQuantity);
+
+//zaznacz checkbox
+const discountShouldBeEnabled =
+  +discountElement.dataset.discountShouldBeEnabled;
+
+if (discountShouldBeEnabled) {
+  discountCheckbox.click();
+}
